85c82a9a8b22f8b45e2ffc617b3ac0e600d33501
IGLOPAS_REYMER <iglopasreymer_bsit@plmun.edu.ph>
2025-02-26 18:51:26 +0800

    Add files via upload

---
+++ newactivity.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string name;
+int birthyear;
+
+cout<<"What is your name? "<<endl;
+cin>>name;
+cout<<"What is the year of your birthyear? "<<endl;
+cin>>birthyear;
+ int age = 2025 - birthyear;
+cout<<"Your name is "<<name<<" and then your birthday is "<<birthyear<<" so your age is "<<age;
+
+return 0;
+
+
+
+}
